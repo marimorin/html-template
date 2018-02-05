21b0316fcc53a93e7122bef170ad75c5a25ebdab
moriyamakazunori <moriyama@marimo-tech.com>
2018-02-05 19:57:33 +0900

    Bootstrap

--- src/js/vendor/index.js
+++ src/js/vendor/index.js
@@ -1,2 +1,2 @@
 import '../../styles/vendor/index.scss'
-import 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
+import 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,19 @@ const cleanOptions = { exclude:  [] }
 let plugins = [
   new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }),
   new webpack.ProvidePlugin({ jQuery: "jquery", $: "jquery" }),
+  new CleanWebpackPlugin(pathsToClean, cleanOptions),
 ]
 
 switch (process.env.NODE_ENV) {
   case "development":
     plugins = plugins.concat([
       new ExtractTextPlugin({ filename: "css/[name].css" }),
-      new CleanWebpackPlugin(pathsToClean, cleanOptions),
     ])
     break
   case "production":
     plugins = plugins.concat([
       new ExtractTextPlugin({ filename: "css/[name].css" }),
       new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-      new CleanWebpackPlugin(pathsToClean, cleanOptions),
     ])
     break
   case "hot":
